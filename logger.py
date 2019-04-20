8f96d1a05a664cc90cea4edfabf4367d9829432a
Constuelo <ferguson.rory@gmail.com>
2019-04-20 20:02:37 +0100

    temperature_reading function added

--- logger.py
+++ logger.py
@@ -1,2 +1,18 @@
-import envirophat
+from envirophat import weather
 
+
+def temperature_reading():
+    """ Retrieve temperature in celsius
+        :return: temperature
+        :rtype: float
+    """
+    temperature = weather.temperature()
+    return "{0:.2f}".format(temperature)
+
+
+def main():
+    print(temperature_reading())
+
+
+if __name__ == '__main__':
+    main()
