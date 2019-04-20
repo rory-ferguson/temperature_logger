629f2d67120a59aa137ba44d20d790a1c06b3138
Constuelo <ferguson.rory@gmail.com>
2019-04-20 19:39:20 +0100

    Update

--- connect.py
+++ connect.py
@@ -1,43 +1,53 @@
 import sqlite3
+import sys
+
 
 def connect(database):
-	""" Create a connection to a database
-		:param database: database name
-		:return: conn
-	"""
-	try:
-		conn = sqlite3.connect(database)
-		return conn
-	except Exception as e:
-		print(e)
+    """ Create a connection to a database
+        :param database: database name
+        :return: conn
+    """
+    try:
+        conn = sqlite3.connect(database)
+        return conn
+    except Exception as e:
+        print(e)
+        sys.exit()
 
-	return None
+    return None
 
 
 def write(database, temp):
-	""" Log entry to the database
-		:param database: database object
-		:param temp: temperature reading
-		:return: id of commited row
-	"""
-	c = database.cursor()
-	c.execute("INSERT INTO temps values(datetime('now'), (?))", (temp,))
-	database.commit()
-	return c.lastrowid
+    """ Log entry to the database
+        :param database: database object
+        :param temp: temperature reading
+        :return: id of commited row
+        :rtype: int
+    """
+    c = database.cursor()
+    try:
+        c.execute("INSERT INTO temps values(datetime('now'), (?))", (temp,))
+    except Exception as e:
+        print(e)
+            
+    database.commit()
+    return c.lastrowid
+    
 
 
 def close(database):
-	""" Closes the database connection
-		:param database: database object
-	"""	
-	database.close()
+    """ Closes the database connection
+        :param database: database object
+    """
+    database.close()
 
 
 def main():
-	conn = connect('temperatue.db')
-	write(conn, "22")
-	close(conn)
+    conn = connect('temperature.db')
+    write(conn, "22")
+    close(conn)
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
